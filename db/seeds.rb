f36fbe706ae61736cb0a50c003d7a6c91f819d1b
Lizzie <lizziewrab@me.com>
2022-09-15 21:33:36 -0500

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,9 @@ Exercise.create(name: "Power Walking", instructions: "Walk at a speed of 4-5.5 m
 Exercise.create(name: "Interval arms", instructions: "holding 5lb weights do 10 counts of butterfly arms, then arm curls, then overhead arm curls, then lateral raise. cycle through each set at least 3 times. increase weights as necessary .")
 
 
-Workout.create(name: "Monday", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.")
-Workout.create(name: "Tuesday", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.")
-Workout.create(name: "Wednesday", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.")
-Workout.create(name: "Thursday", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.")
-Workout.create(name: "Friday", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.")
-Workout.create(name: "Saturday", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.")
-Workout.create(name: "Sunday ", directions: "Rest Day!")
+Workout.create(name: "Lizzie", directions: "Light Endurance and strength day. 30 min of endurance and 5 sets of strength training.")
+Workout.create(name: "Ally", directions: "Orange Theory Themed Workout.")
+Workout.create(name: "Natalie", directions: "One hour Yoga Flow.")
+
 
 puts "✅ Done seeding!"
