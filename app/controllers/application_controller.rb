2b24fcb896cb9e20606afe9e130ff31658823fb5
Lizzie <lizziewrab@me.com>
2022-09-28 17:26:08 -0500

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,6 @@ class ApplicationController < Sinatra::Base
     workouts.to_json(include: :exercises)
   end
 
-  # # get'/workouts/:id' do
-  #   workouts = Workout.all
-  #   workouts.to_json(include: :exercises)
-  # end
-
   get "/exercises" do
     exercises = Exercise.all
     exercises.to_json
